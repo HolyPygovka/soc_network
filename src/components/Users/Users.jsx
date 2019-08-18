75fd6f4afc4f4d85adaa726fb640ad521805d518
unknown <r2598509@gmail.com>
2019-08-18 14:50:26 +0300

    51 axios get users from API

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,54 +1,17 @@
 import React from 'react';
 import s from './Users.module.css'
+import * as axios from 'axios';
+import userPhoto from '../../assets/images/user.png';
 
 let Users = (props) => {
 
-    if (props.users.length == 0) {
-        props.setUsers( [
-            {
-              id: 1,
-              photoUrl: 'https://pbs.twimg.com/profile_images/950068518313017344/vY9UQRFH_400x400.jpg',
-              followed: false,
-              fullName: 'Alex',
-              status: 'I like cats',
-              location: {
-                city: 'Minsk',
-                country: 'Belarus'
-              }
-            }, {
-              id: 2,
-              photoUrl: 'https://pbs.twimg.com/profile_images/950068518313017344/vY9UQRFH_400x400.jpg',
-              followed: true,
-              fullName: 'Victor',
-              status: 'I play',
-              location: {
-                city: 'Brest',
-                country: 'Belarus'
-              }
-            }, {
-              id: 3,
-              photoUrl: 'https://pbs.twimg.com/profile_images/950068518313017344/vY9UQRFH_400x400.jpg',
-              followed: true,
-              fullName: 'John',
-              status: 'I am bissy',
-              location: {
-                city: 'Minsk',
-                country: 'Belarus'
-              }
-            }, {
-              id: 4,
-              photoUrl: 'https://pbs.twimg.com/profile_images/950068518313017344/vY9UQRFH_400x400.jpg',
-              followed: false,
-              fullName: 'Lila',
-              status: 'I am happy',
-              location: {
-                city: 'Moscow',
-                country: 'Russia'
-              }
-            }
-          ]);
-    }
-
+  if (props.users.length === 0) {
+    axios
+      .get("https://social-network.samuraijs.com/api/1.0/users")
+      .then(response => {
+        props.setUsers(response.data.items)
+      });
+  }
 
   return <div>
     {props
@@ -56,7 +19,12 @@ let Users = (props) => {
       .map(el => <div key={el.id}>
         <span>
           <div>
-            <img src={el.photoUrl} className={s.user_img} alt=""/>
+            <img
+              src={el.photos.small != null
+              ? el.photos.small
+              : userPhoto}
+              className={s.user_img}
+              alt=""/>
           </div>
           <div>
             {el.followed
@@ -71,12 +39,12 @@ let Users = (props) => {
         </span>
         <span>
           <span>
-            <div>{el.fullName}</div>
+            <div>{el.name}</div>
             <div>{el.status}</div>
           </span>
           <span>
-            <div>{el.location.country}</div>
-            <div>{el.location.city}</div>
+            <div>{"el.location.country"}</div>
+            <div>{"el.location.city"}</div>
           </span>
         </span>
       </div>)
