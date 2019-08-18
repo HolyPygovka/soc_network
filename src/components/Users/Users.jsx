1408de446d6bac73791107b988a0b660688590f1
unknown <r2598509@gmail.com>
2019-08-18 17:01:33 +0300

    54 life cycle ComponentDidMount

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -5,15 +5,14 @@ import userPhoto from '../../assets/images/user.png';
 
 class Users extends React.Component {
 
-  constructor(props) {
-    super(props);
+  componentDidMount() {
     axios
-      .get("https://social-network.samuraijs.com/api/1.0/users")
-      .then(response => {
-        this
-          .props
-          .setUsers(response.data.items)
-      });
+    .get("https://social-network.samuraijs.com/api/1.0/users")
+    .then(response => {
+      this
+        .props
+        .setUsers(response.data.items)
+    });
   }
   
   render() {
