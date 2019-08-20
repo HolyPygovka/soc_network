2b341679d8c59139f658f12bd650ae27b3776eac
unknown <r2598509@gmail.com>
2019-08-20 04:07:22 +0300

    74 componentDidUpdate

--- src/components/Profile/Profileinfo/ProfileStatus.jsx
+++ src/components/Profile/Profileinfo/ProfileStatus.jsx
@@ -22,12 +22,20 @@ class ProfileStatus extends React.Component {
         });
     }
 
+    componentDidUpdate(prevProps, prevState) {
+        if (prevProps.status !== this.props.status) {
+            this.setState({
+                status: this.props.status
+            });
+        }
+    }
+
     render() {
         return (
             <div>
                 {!this.state.editMode &&
                     <div>
-                        <span onDoubleClick={this.activateEditMode}>{this.props.status}</span>
+                        <span onDoubleClick={this.activateEditMode}>{this.props.status || "enter your status"}</span>
                     </div>
                 }
                 {this.state.editMode &&
